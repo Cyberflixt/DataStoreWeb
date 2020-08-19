30908fbf9c95548b1baed828574f12ac84a4e676
Cyberflixt <cyberflixt@gmail.com>
2020-08-19 18:58:22 +0200

    new

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const Datastore = require('nedb');
 const express = require('express');
 require('dotenv').config();
 
+const port = process.env.PORT || 9
 const app = express();
-app.listen(3000, ()=> console.log('listening at 3000'));
+app.listen(port, ()=> {
+    console.log(`Starting server at ${port}`)
+});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb'}));
 
